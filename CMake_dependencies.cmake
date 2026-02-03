7870a3817976453f181386f6677f517bc84ba811
Michal Lurie <Michal.Lurie@gmail.com>
2026-02-03 23:20:21 +0100

    CMake: Further attempts to fix MSVC build

--- CMake_dependencies.cmake
+++ CMake_dependencies.cmake
@@ -18,13 +18,11 @@ if(PkgConfig_FOUND)
     message(STATUS "PkgConfig did not find zlib. ots project will try to build and link static version of zlib.")
   endif()
 else()
-  message(STATUS "Could NOT find PkgConfig. ots project will try to build and link static version of zlib.")
+  message(STATUS "Could NOT find PkgConfig. ots project will try to build and link static version of zlib.\n")
 endif()
 
 # If we cannot find it using PkgConfig, we build a static version from source
 if(NOT TARGET PkgConfig::ZLIB_PC)
-  set(__BACKUP_CPM_USE_LOCAL_PACKAGES ${CPM_USE_LOCAL_PACKAGES})
-  set(CPM_USE_LOCAL_PACKAGES OFF)
   CPMAddPackage(
     URI "gh:madler/zlib@1.3.1.2"
     OPTIONS
@@ -33,9 +31,6 @@ if(NOT TARGET PkgConfig::ZLIB_PC)
     "ZLIB_BUILD_TESTING OFF"
     NAME ZLIB
   )
-  set(CPM_USE_LOCAL_PACKAGES ${__BACKUP_CPM_USE_LOCAL_PACKAGES})
-  unset(__BACKUP_CPM_USE_LOCAL_PACKAGES)
-  add_library(ZLIB::ZLIB ALIAS zlibstatic)
 endif()
 
 # WOFF2
