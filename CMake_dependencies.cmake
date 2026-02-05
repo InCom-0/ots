99f5e4cde3aa6b5eaaf5106d78483a438304f14f
Michal Lurie <Michal.Lurie@gmail.com>
2026-02-05 15:36:38 +0100

    Fix: Typo

--- CMake_dependencies.cmake
+++ CMake_dependencies.cmake
@@ -2,7 +2,7 @@ if(NOT DEFINED CPM_USE_LOCAL_PACKAGES)
     set(CPM_USE_LOCAL_PACKAGES ${ots_USE_LOCAL_PACKAGES} CACHE BOOL "CPM will try to find packages locally first" FORCE)
 endif()
 if(NOT DEFINED CPM_LOCAL_PACKAGES_ONLY)
-    set(CPM_LOCAL_PACKAGES_ONLY ${ots_USE_LOCAL_PACKAGES} CACHE BOOL
+    set(CPM_LOCAL_PACKAGES_ONLY ${ots_USE_LOCAL_PACKAGES_ONLY} CACHE BOOL
         "CPM will not be forbidden from downloading packages. Will have to use local packages." FORCE)
 endif()
 
