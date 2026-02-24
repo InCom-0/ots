6e87d6d736a477e67b3d64f0a0f31952dddeafcd
Michal Lurie <Michal.Lurie@gmail.com>
2026-02-24 13:55:37 +0100

    Feat: Heavy upgrade of all things CMake

--- CMake_dependencies.cmake
+++ CMake_dependencies.cmake
@@ -1,10 +1,3 @@
-if(NOT DEFINED CPM_USE_LOCAL_PACKAGES)
-    set(CPM_USE_LOCAL_PACKAGES ${ots_USE_LOCAL_PACKAGES} CACHE BOOL "CPM will try to find packages locally first" FORCE)
-endif()
-if(NOT DEFINED CPM_LOCAL_PACKAGES_ONLY)
-    set(CPM_LOCAL_PACKAGES_ONLY ${ots_USE_LOCAL_PACKAGES_ONLY} CACHE BOOL
-        "CPM will not be forbidden from downloading packages. Will have to use local packages." FORCE)
-endif()
 
 include(cmake/CPM.cmake)
 
@@ -12,47 +5,43 @@ include(cmake/CPM.cmake)
 # We need to provide custom CMake module/script for WOFF2 (FindWOFF2.cmake)
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/cmake/modules")
 
-# ZLIB provides .pc so we prefer to find it using PkgConfig
-set(PKG_CONFIG_USE_STATIC_LIBS ON)
-find_package(PkgConfig QUIET)
-if(PkgConfig_FOUND)
-    # Check the module name with: pkg-config --modversion zlib
-    pkg_check_modules(ZLIB_PC IMPORTED_TARGET zlib)
-    if(NOT TARGET PkgConfig::ZLIB_PC)
-        message(STATUS "PkgConfig did not find zlib. ots project will try to build and link static version of zlib.")
+
+if(ots_BUILD_SHARED_LIB)
+    set(ots_BUILD_STATIC_LIB OFF)
+else()
+    set(ots_BUILD_STATIC_LIB ON)
+endif()
+
+set(ZLIB_USE_STATIC_LIBS ${ots_BUILD_STATIC_LIB}) ### Note: This is useful for FindZLIB.cmake only
+CPMAddPackage(
+    URI "gh:madler/zlib@1.3.1"
+    OPTIONS
+    "ZLIB_BUILD_SHARED ${ots_BUILD_SHARED_LIB}"
+    "ZLIB_BUILD_STATIC ${ots_BUILD_STATIC_LIB}"
+    "ZLIB_INSTALL OFF"
+    "ZLIB_INSTALL_COMPAT_DLL OFF"
+    "ZLIB_BUILD_TESTING OFF"
+    "ZLIB_BUILD_MINIZIP OFF"
+    "ZLIB_BUILD_EXAMPLES OFF"
+    NAME ZLIB
+)
+unset(ZLIB_USE_STATIC_LIBS)
+
+if(ZLIB_ADDED AND (NOT TARGET ZLIB::ZLIB))
+    if(ots_BUILD_STATIC_LIB)
+        add_library(ZLIB::ZLIB ALIAS zlibstatic)
+    else()
+        add_library(ZLIB::ZLIB ALIAS zlib)
     endif()
-else()
-    message(STATUS "Could NOT find PkgConfig. ots project will try to build and link static version of zlib.\n")
 endif()
 
-# If we cannot find it using PkgConfig, we build a static version from source
-if(NOT TARGET PkgConfig::ZLIB_PC)
-    CPMAddPackage(
-        URI "gh:madler/zlib@1.3.1.2"
-        OPTIONS
-        "ZLIB_BUILD_SHARED OFF"
-        "ZLIB_INSTALL OFF"
-        "ZLIB_BUILD_TESTING OFF"
-        NAME ZLIB
-    )
-endif()
+CPMAddPackage(
+    URI "gh:InCom-0/woff2#otfccxx"
+    OPTIONS
+    "NOISY_LOGGING OFF"
+    "BUILD_SHARED_LIBS ${ots_BUILD_SHARED_LIB}"
+    NAME WOFF2
+    FIND_PACKAGE_ARGUMENTS "COMPONENTS woff2dec woff2enc"
+)
 
-# WOFF2
-if(WIN32 AND (CMAKE_BUILD_TYPE STREQUAL "Release"))
-    set(CPM_USE_LOCAL_PACKAGES OFF)
-    CPMAddPackage(
-        URI "gh:InCom-0/woff2#otfccxx"
-        OPTIONS "NOISY_LOGGING OFF" "BUILD_SHARED_LIBS OFF"
-        NAME WOFF2
-    )
-    unset(CPM_USE_LOCAL_PACKAGES)
-else()
 
-endif()
-if(WOFF2_ADDED)
-    set(OTS_WOFF2_BUILDFROMSOURCE TRUE)
-endif()
-
-if(ZLIB_ADDED)
-    set(OTS_ZLIB_BUILDFROMSOURCE TRUE)
-endif()
--- cmake/modules/FindWOFF2.cmake
+++ cmake/modules/FindWOFF2.cmake
@@ -69,24 +69,24 @@ find_library(WOFF2_LIBRARY
 # There's nothing in the WOFF2 headers that could be used to detect the exact
 # WOFF2 version being used so don't attempt to do so. A version can only be found
 # through pkg-config
-if ("${WOFF2_FIND_VERSION}" VERSION_GREATER "${WOFF2_VERSION}")
-    if (WOFF2_VERSION)
+if("${WOFF2_FIND_VERSION}" VERSION_GREATER "${WOFF2_VERSION}")
+    if(WOFF2_VERSION)
         message(FATAL_ERROR "Required version (" ${WOFF2_FIND_VERSION} ") is higher than found version (" ${WOFF2_VERSION} ")")
-    else ()
+    else()
         message(WARNING "Cannot determine WOFF2 version without pkg-config")
-    endif ()
-endif ()
+    endif()
+endif()
 
 # Find components
-if (WOFF2_LIBRARY)
+if(WOFF2_LIBRARY)
     set(_WOFF2_REQUIRED_LIBS_FOUND ON)
     set(WOFF2_LIBS_FOUND "WOFF2 (required): ${WOFF2_LIBRARY}")
-else ()
+else()
     set(_WOFF2_REQUIRED_LIBS_FOUND OFF)
     set(WOFF2_LIBS_NOT_FOUND "WOFF2 (required)")
-endif ()
+endif()
 
-if ("dec" IN_LIST WOFF2_FIND_COMPONENTS)
+if("woff2dec" IN_LIST WOFF2_FIND_COMPONENTS)
     pkg_check_modules(PC_WOFF2_DEC libwoff2dec)
 
     find_path(WOFF2_INCLUDE_DIR
@@ -99,36 +99,65 @@ if ("dec" IN_LIST WOFF2_FIND_COMPONENTS)
         HINTS ${PC_WOFF2_DEC_LIBDIR} ${PC_WOFF2_DEC_LIBRARY_DIRS}
     )
 
-    if (WOFF2_DEC_LIBRARY)
-        if (WOFF2_FIND_REQUIRED_dec)
-            list(APPEND WOFF2_LIBS_FOUND "dec (required): ${WOFF2_DEC_LIBRARY}")
-        else ()
-           list(APPEND WOFF2_LIBS_FOUND "dec (optional): ${WOFF2_DEC_LIBRARY}")
-        endif ()
-    else ()
-        if (WOFF2_FIND_REQUIRED_dec)
-           set(_WOFF2_REQUIRED_LIBS_FOUND OFF)
-           list(APPEND WOFF2_LIBS_NOT_FOUND "dec (required)")
-        else ()
-           list(APPEND WOFF2_LIBS_NOT_FOUND "dec (optional)")
-        endif ()
-    endif ()
-endif ()
+    if(WOFF2_DEC_LIBRARY)
+        if(WOFF2_FIND_REQUIRED_woff2dec)
+            list(APPEND WOFF2_LIBS_FOUND "woff2dec (required): ${WOFF2_DEC_LIBRARY}")
+        else()
+            list(APPEND WOFF2_LIBS_FOUND "woff2dec (optional): ${WOFF2_DEC_LIBRARY}")
+        endif()
+    else()
+        if(WOFF2_FIND_REQUIRED_woff2dec)
+            set(_WOFF2_REQUIRED_LIBS_FOUND OFF)
+            list(APPEND WOFF2_LIBS_NOT_FOUND "woff2dec (required)")
+        else()
+            list(APPEND WOFF2_LIBS_NOT_FOUND "woff2dec (optional)")
+        endif()
+    endif()
+endif()
 
-if (NOT WOFF2_FIND_QUIETLY)
-    if (WOFF2_LIBS_FOUND)
+if("woff2enc" IN_LIST WOFF2_FIND_COMPONENTS)
+    pkg_check_modules(PC_WOFF2_ENC libwoff2enc)
+
+    find_path(WOFF2_INCLUDE_DIR
+        NAMES woff2/encode.h
+        HINTS ${PC_WOFF2_ENC_INCLUDEDIR} ${PC_WOFF2_ENC_INCLUDE_DIRS}
+    )
+
+    find_library(WOFF2_ENC_LIBRARY
+        NAMES ${WOFF2_ENC_NAMES} woff2enc
+        HINTS ${PC_WOFF2_ENC_LIBDIR} ${PC_WOFF2_ENC_LIBRARY_DIRS}
+    )
+
+    if(WOFF2_ENC_LIBRARY)
+        if(WOFF2_FIND_REQUIRED_woff2enc)
+            list(APPEND WOFF2_LIBS_FOUND "woff2enc (required): ${WOFF2_ENC_LIBRARY}")
+        else()
+            list(APPEND WOFF2_LIBS_FOUND "woff2enc (optional): ${WOFF2_ENC_LIBRARY}")
+        endif()
+    else()
+        if(WOFF2_FIND_REQUIRED_woff2enc)
+            set(_WOFF2_REQUIRED_LIBS_FOUND OFF)
+            list(APPEND WOFF2_LIBS_NOT_FOUND "woff2enc (required)")
+        else()
+            list(APPEND WOFF2_LIBS_NOT_FOUND "woff2enc (optional)")
+        endif()
+    endif()
+endif()
+
+if(NOT WOFF2_FIND_QUIETLY)
+    if(WOFF2_LIBS_FOUND)
         message(STATUS "Found the following WOFF2 libraries:")
-        foreach (found ${WOFF2_LIBS_FOUND})
+        foreach(found ${WOFF2_LIBS_FOUND})
             message(STATUS " ${found}")
-        endforeach ()
-    endif ()
-    if (WOFF2_LIBS_NOT_FOUND)
+        endforeach()
+    endif()
+    if(WOFF2_LIBS_NOT_FOUND)
         message(STATUS "The following WOFF2 libraries were not found:")
-        foreach (found ${WOFF2_LIBS_NOT_FOUND})
+        foreach(found ${WOFF2_LIBS_NOT_FOUND})
             message(STATUS " ${found}")
-        endforeach ()
-    endif ()
-endif ()
+        endforeach()
+    endif()
+endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(WOFF2
@@ -137,30 +166,40 @@ find_package_handle_standard_args(WOFF2
     VERSION_VAR WOFF2_VERSION
 )
 
-if (WOFF2_LIBRARY AND NOT TARGET WOFF2::common)
+if(WOFF2_LIBRARY AND NOT TARGET WOFF2::common)
     add_library(WOFF2::common UNKNOWN IMPORTED GLOBAL)
     set_target_properties(WOFF2::common PROPERTIES
         IMPORTED_LOCATION "${WOFF2_LIBRARY}"
         INTERFACE_COMPILE_OPTIONS "${WOFF2_COMPILE_OPTIONS}"
     )
-endif ()
+endif()
 
-if (WOFF2_DEC_LIBRARY AND NOT TARGET WOFF2::dec)
-    add_library(WOFF2::dec UNKNOWN IMPORTED GLOBAL)
-    set_target_properties(WOFF2::dec PROPERTIES
+if(WOFF2_DEC_LIBRARY AND NOT TARGET WOFF2::woff2dec)
+    add_library(WOFF2::woff2dec UNKNOWN IMPORTED GLOBAL)
+    set_target_properties(WOFF2::woff2dec PROPERTIES
         IMPORTED_LOCATION "${WOFF2_DEC_LIBRARY}"
-        INTERFACE_COMPILE_OPTIONS "${WOFF2_COMPILE_OPTIONS}"
-        INTERFACE_INCLUDE_DIRECTORIES "${WOFF2_DEC_INCLUDE_DIR}"
+        INTERFACE_COMPILE_OPTIONS "${PC_WOFF2_DEC_CFLAGS_OTHER}"
+        INTERFACE_INCLUDE_DIRECTORIES "${WOFF2_INCLUDE_DIR}"
     )
-endif ()
+endif()
+
+if(WOFF2_ENC_LIBRARY AND NOT TARGET WOFF2::woff2enc)
+    add_library(WOFF2::woff2enc UNKNOWN IMPORTED GLOBAL)
+    set_target_properties(WOFF2::woff2enc PROPERTIES
+        IMPORTED_LOCATION "${WOFF2_ENC_LIBRARY}"
+        INTERFACE_COMPILE_OPTIONS "${PC_WOFF2_ENC_CFLAGS_OTHER}"
+        INTERFACE_INCLUDE_DIRECTORIES "${WOFF2_INCLUDE_DIR}"
+    )
+endif()
 
 mark_as_advanced(
     WOFF2_LIBRARY
-    WOFF2_DEC_INCLUDE_DIR
+    WOFF2_INCLUDE_DIR
     WOFF2_DEC_LIBRARY
+    WOFF2_ENC_LIBRARY
 )
 
-if (WOFF2_FOUND)
-    set(WOFF2_LIBRARIES ${WOFF2_LIBRARY} ${WOFF2_DEC_LIBRARY})
+if(WOFF2_FOUND)
+    set(WOFF2_LIBRARIES ${WOFF2_LIBRARY} ${WOFF2_DEC_LIBRARY} ${WOFF2_ENC_LIBRARY})
     set(WOFF2_INCLUDE_DIRS ${WOFF2_INCLUDE_DIR})
-endif ()
+endif()
