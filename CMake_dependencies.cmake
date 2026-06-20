077de2368eadb31b22554e82991ef41e5a503d3b
Michal Lurie <Michal.Lurie@gmail.com>
2026-06-20 09:15:02 +0200

    Fix: zlib not found somehow

--- CMake_dependencies.cmake
+++ CMake_dependencies.cmake
@@ -12,21 +12,25 @@ else()
     set(OTS_BUILD_STATIC_LIB ON)
 endif()
 
-CPMAddPackage(
-    URL https://github.com/madler/zlib/releases/download/v1.3.2/zlib-1.3.2.tar.xz
-    URL_HASH SHA256=d7a0654783a4da529d1bb793b7ad9c3318020af77667bcae35f95d0e42a792f3
-    EXCLUDE_FROM_ALL TRUE
-    OPTIONS
-    "ZLIB_BUILD_SHARED ${OTS_BUILD_SHARED_LIB}"
-    "ZLIB_BUILD_STATIC ${OTS_BUILD_STATIC_LIB}"
-    "ZLIB_INSTALL OFF"
-    "ZLIB_INSTALL_COMPAT_DLL OFF"
-    "ZLIB_BUILD_TESTING OFF"
-    "ZLIB_BUILD_MINIZIP OFF"
-    "ZLIB_BUILD_EXAMPLES OFF"
-    VERSION 1.3.2
-    NAME ZLIB
-)
+find_package(ZLIB)
+
+if (NOT ZLIB_FOUND)
+    CPMAddPackage(
+        URL https://github.com/madler/zlib/releases/download/v1.3.2/zlib-1.3.2.tar.xz
+        URL_HASH SHA256=d7a0654783a4da529d1bb793b7ad9c3318020af77667bcae35f95d0e42a792f3
+        EXCLUDE_FROM_ALL TRUE
+        OPTIONS
+        "ZLIB_BUILD_SHARED ${OTS_BUILD_SHARED_LIB}"
+        "ZLIB_BUILD_STATIC ${OTS_BUILD_STATIC_LIB}"
+        "ZLIB_INSTALL OFF"
+        "ZLIB_INSTALL_COMPAT_DLL OFF"
+        "ZLIB_BUILD_TESTING OFF"
+        "ZLIB_BUILD_MINIZIP OFF"
+        "ZLIB_BUILD_EXAMPLES OFF"
+        VERSION 1.3.2
+        NAME ZLIB
+    )
+endif()
 unset(ZLIB_USE_STATIC_LIBS)
 
 if(ZLIB_ADDED AND (NOT TARGET ZLIB::ZLIB))
